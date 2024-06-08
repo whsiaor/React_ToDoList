6e27a855562ce5ac2b664770dbeaeb7ca9758b33
Sean Wang <seanwang@Seans-MacBook-Air.local>
2024-06-09 09:24:43 +1000

    v.1.0 update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const initialList = [
+  { id: 1, text: "Learn React" },
+  { id: 2, text: "Use React" },
+  { id: 3, text: "Master React" },
+];
+export default function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDoList, setToDoList] = useState(initialList);
 
-function App() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!toDo) return;
+    const newList = [{ id: Date.now(), text: toDo }, ...toDoList];
+    setToDoList(newList);
+    setToDo("");
+  }
+
+  function clearAll() {
+    setToDoList([]);
+  }
+  function handleDelete(id) {
+    setToDoList((list) => list.filter((todo) => todo.id !== id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <TextArea
+        handleSubmit={handleSubmit}
+        toDo={toDo}
+        onSetToDo={setToDo}
+        clearAll={clearAll}
+      >
+        - To Do List -
+      </TextArea>
+      <ToDoList onDelete={handleDelete} toDoList={toDoList} />
     </div>
   );
 }
 
-export default App;
+function TextArea({ children, toDo, onSetToDo, handleSubmit, clearAll }) {
+  return (
+    <div className="textarea">
+      <h1 className="h1">{children}</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          value={toDo}
+          onChange={(e) => onSetToDo(e.target.value)}
+          type="text"
+          placeholder="Write something..."
+        ></input>
+
+        <button className="add" onClick={handleSubmit}>+</button>
+        {/* <Button onClick={clearAll}>ClearAll</Button> */}
+      </form>
+    </div>
+  );
+}
+
+function ToDoList({ toDoList, onDelete }) {
+  return (
+    <div className="todolist">
+      <ul>
+        {toDoList.map((todo) => (
+          <li className="list" key={todo.id}>
+            <Button onClick={() => onDelete(todo.id)}>✔</Button>
+            <span className="text">{todo.text}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="toggle" type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
